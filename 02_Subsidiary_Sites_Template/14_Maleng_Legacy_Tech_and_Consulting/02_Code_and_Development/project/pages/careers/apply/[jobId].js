aa322a925d592c68b409c4d669e9aa9d117980c6
MASINGITA OTTIS MALULEKE <216135982@edu.vut.ac.za>
2026-08-11 09:07:26 +0200

    fixing Tech style

--- 02_Subsidiary_Sites_Template/14_Maleng_Legacy_Tech_and_Consulting/02_Code_and_Development/project/pages/careers/apply/[jobId].js
+++ 02_Subsidiary_Sites_Template/14_Maleng_Legacy_Tech_and_Consulting/02_Code_and_Development/project/pages/careers/apply/[jobId].js
@@ -1,3 +1,21 @@
+import React from 'react';
+import Head from 'next/head';
+import Navigation from '../../../../components/Navigation';
+import Footer from '../../../../components/Footer';
+
 export default function JobApplication() {
-  return <div>Job Application</div>;
+  return (
+    <>
+      <Head>
+        <title>Apply for Job</title>
+      </Head>
+      <Navigation />
+      <div className="min-h-screen bg-primary">
+        <div className="container mx-auto px-6 py-20">
+          <h1 className="text-4xl font-bold text-white">Job Application</h1>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
--- 02_Subsidiary_Sites_Template/14_Maleng_Legacy_Tech_and_Consulting/02_Code_and_Development/project/pages/resources/index.js
+++ 02_Subsidiary_Sites_Template/14_Maleng_Legacy_Tech_and_Consulting/02_Code_and_Development/project/pages/resources/index.js
@@ -1,3 +1,21 @@
+import React from 'react';
+import Head from 'next/head';
+import Navigation from '../../components/Navigation';
+import Footer from '../../components/Footer';
+
 export default function Resources() {
-  return <div>Resources</div>;
+  return (
+    <>
+      <Head>
+        <title>Resources</title>
+      </Head>
+      <Navigation />
+      <div className="min-h-screen bg-primary">
+        <div className="container mx-auto px-6 py-20">
+          <h1 className="text-4xl font-bold text-white">Resources</h1>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
